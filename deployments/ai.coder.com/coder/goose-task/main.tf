d9f17f454fcc1aeb819db8c85a349e06b904110e
Jullian Pepito <jullian@coder.com>
2025-08-14 11:47:02 -0500

    fix(goose-task): Ties cost to variable instead of local.

--- deployments/ai.coder.com/coder/goose-task/main.tf
+++ deployments/ai.coder.com/coder/goose-task/main.tf
@@ -26,6 +26,11 @@ variable "gh_username" {
     sensitive = true
 }
 
+variable "cost" {
+    type = string
+    default = 2
+}
+
 data "coder_parameter" "ai_prompt" {
     type        = "string"
     name        = "AI Prompt"
@@ -90,7 +95,7 @@ locals {
 resource "coder_metadata" "pod_info" {
     count = data.coder_workspace.me.start_count
     resource_id = kubernetes_pod.dev[0].id
-    daily_cost = local.cost
+    daily_cost = var.cost
     item {
         key   = "UUID"
         value = random_uuid.prebuilds.result
@@ -105,7 +110,6 @@ data "coder_workspace" "me" {}
 data "coder_workspace_owner" "me" {}
 
 locals {
-    cost = 2
     home_folder = "/home/coder"
     work_folder = data.coder_parameter.git-repo.value == "" ? local.home_folder : join("/", [
         local.home_folder, element(split(".", element(split("/", data.coder_parameter.git-repo.value), -1)), 0)
@@ -348,7 +352,7 @@ locals {
         GIT_AUTHOR_NAME = data.coder_workspace_owner.me.name
         GIT_AUTHOR_EMAIL = data.coder_workspace_owner.me.email
         GH_TOKEN = local.logged_into_git ? data.coder_external_auth.github.access_token : var.gh_token
-        NODE_OPTIONS = "--max-old-space-size=${512*local.cost}"
+        NODE_OPTIONS = "--max-old-space-size=${512*var.cost}"
     }
 }
 
@@ -415,8 +419,8 @@ resource "kubernetes_pod" "dev" {
             }
             resources {
                 limits = {
-                    cpu = "${ceil(local.cost/2)}"
-                    memory = "${local.cost}G"
+                    cpu = "${ceil(var.cost/2)}"
+                    memory = "${var.cost}G"
                 }
             }
             security_context {
