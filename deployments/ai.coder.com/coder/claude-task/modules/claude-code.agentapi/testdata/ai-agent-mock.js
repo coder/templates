2ff28fd6d598877f0207f6159e7641738ee36e5f
Jullian Pepito <jullian@MacBook-Pro.local>
2025-09-30 00:04:03 -0700

    feat(claude-task): Updates Claude Task template w/ module upgrades/replacements.

--- deployments/ai.coder.com/coder/claude-task/modules/claude-code.agentapi/testdata/ai-agent-mock.js
+++ deployments/ai.coder.com/coder/claude-task/modules/claude-code.agentapi/testdata/ai-agent-mock.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const main = async () => {
-  console.log("mocking claude");
+  console.log("mocking an ai agent");
   // sleep for 30 minutes
   await new Promise((resolve) => setTimeout(resolve, 30 * 60 * 1000));
 };
